806317aa84fab6d752799adb749f33f9622940d2
Shamsuddin-Memon <shamsuddinmemon.77@gmail.com>
2024-01-30 21:02:31 +0500

    This is checking for try

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -110,14 +110,17 @@ export default function About() {
             data-bs-parent="#accordionExample"
           >
             <div className="accordion-body" style={myStyle}>
-              <strong>This is the third item's accordion body.</strong> It is
-              hidden by default, until the collapse plugin adds the appropriate
-              classNamees that we use to style each element. These classNamees
-              control the overall appearance, as well as the showing and hiding
-              via CSS transitions. You can modify any of this with custom CSS or
-              overriding our default variables. It's also worth noting that just
-              about any HTML can go within the <code>.accordion-body</code>,
-              though the transition does limit overflow.
+              <strong>
+                This is the third item's accordion <p>to</p> body.
+              </strong>{" "}
+              It is hidden by default, until the collapse plugin adds the
+              appropriate classNamees that we use to style each element. These
+              classNamees control the overall appearance, as well as the showing
+              and hiding via CSS transitions. You can modify any of this with
+              custom CSS or overriding our default variables. It's also worth
+              noting that just about any HTML can go within the{" "}
+              <code>.accordion-body</code>, though the transition does limit
+              overflow.
             </div>
           </div>
         </div>
